c08cb8501d25783d9fbcb284b25649ecb6bb7c4f
Vishal Shah <vishalshah5996@gmail.com>
2022-10-21 19:26:50 +0530

    remove Diamond-Vaults script and css file from index and configure it only for staking page

--- src/pages/staking/index.jsx
+++ src/pages/staking/index.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./staking.css";
 
 const Staking = () => {
   const vaultPath = { vaultpath: 'deez-kits' };
+
+  useEffect(() => {
+    /*<!-- Import Diamond Vault Integration -->*/
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/gh/Diamond-Vaults/dv-embed@latest/build/bundle.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
   return (
       <div id="diamond-vaults" {...vaultPath}></div>
