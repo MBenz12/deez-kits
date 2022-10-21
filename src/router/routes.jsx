ba7a83331839402b859005af0bf8d206d308c105
Koby Ahronson <kobyle69@gmail.com>
2022-10-21 17:40:55 +0300

    - CSS updates - Added staking to main page - Improved structure of routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -25,53 +25,27 @@ const Router = () => {
   return (
     <>
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Suspense fallback={<Loading />}>
+      <Route path="/" element=
           {
-              subDomain === "slotz" ? (<DeezSlotz isMint={true} ref={MusicRef} /> ) :
-              subDomain === "staking" ? (<Staking />) : (<Home />)
-          }
-          </Suspense>
-        }
-      ></Route>
-      <Route
-        path="/mint"
-        element={
-          <Suspense fallback={<Loading />}>
-            <Deezkits isMint={true} ref={MusicRef} isMusicPlayer={true} />
-          </Suspense>
-        }
-      ></Route>
-      <Route
-        path="/deezslotz"
-        element={
-          <Suspense fallback={<Loading />}>
-            <DeezSlotz isMint={true} ref={MusicRef} />
-          </Suspense>
-        }
-      ></Route>
-        <Route
-            path="/staking"
-            element={
-                <Suspense fallback={<Loading />}>
-                    <Staking/>
-                </Suspense>
-            }
-        ></Route>
-      <Route
-        path="/countdown"
-        element={
+              <Suspense fallback={<Loading />}>
+              {
+                  subDomain === "slotz" ? (<DeezSlotz isMint={true} ref={MusicRef} /> ) :
+                  subDomain === "staking" ? (<Staking />) : (<Home />)
+              }
+              </Suspense>
+          }>
+      </Route>
+      <Route path="/mint" element={<Suspense fallback={<Loading />}><Deezkits /></Suspense>}></Route>
+      <Route path="/deezslotz" element={<Suspense fallback={<Loading />}><DeezSlotz /></Suspense>}></Route>
+      <Route path="/staking" element={<Suspense fallback={<Loading />}><Staking/></Suspense>}></Route>
+      <Route path="/countdown" element=
+        {
           <Suspense fallback={<Loading />}>
             <Deezkits isMint={false} ref={MusicRef} isMusicPlayer={true}/>
           </Suspense>
-        }
-      ></Route>
-      <Route
-        path="/discord"
-        element={<External link="https://discord.gg/deezkits" />}
-      ></Route>
+        }>
+      </Route>
+      <Route path="/discord" element={<External link="https://discord.gg/deezkits" />}></Route>
       <Route path="*" element={<Error />}></Route>
     </Routes>
     <Audio ref={MusicRef} />
