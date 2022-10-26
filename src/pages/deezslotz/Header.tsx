413e516f04472acefa19f201a4fdb11eaf8f45e7
dodojon <dodojon.boboev1012@gmail.com>
2022-10-26 12:20:02 -0400

    add mobile responsive

--- src/pages/deezslotz/Header.tsx
+++ src/pages/deezslotz/Header.tsx
@@ -1,5 +1,6 @@
 import {
-  CommunityFunds, PlayerFunds,
+  CommunityFunds,
+  PlayerFunds,
   SolBalance,
   WalletButton
 } from "./HeaderItems";
@@ -22,7 +23,7 @@ const Header = ({
   solBalance: number;
   playerBalance: number;
   prize: number;
-  jackpot:number;
+  jackpot: number;
   won: boolean;
   lost: boolean;
   bet: number;
@@ -53,8 +54,30 @@ const Header = ({
           </div>
         </div>
       </div>
-      <div className="xl:hidden flex flex-col w-full gap-3">
-        <div className="flex flex-col items-center justify-between relative">
+      <div className="xl:hidden flex flex-col w-full gap-3 mb-10">
+        <div className="flex justify-center gap-5 items-center">
+          <div className="sm:w-[224px] flex justify-center">
+            <div className="sm:w-[170px] w-[136px] sm:h-[85px] h-[53px]">
+              <KitsLogo />
+            </div>
+          </div>
+          <WalletButton id={2} />
+        </div>
+        <div className="flex justify-center gap-5">
+          <div className="flex gap-2">
+            <SolBalance id={4} balance={solBalance} lost={lost} bet={bet} />
+          </div>
+          <PlayerFunds
+            won={won}
+            prize={prize}
+            playerBalance={playerBalance}
+            withdrawPlayerMoney={withdrawPlayerMoney}
+          />
+        </div>
+        <div className="flex justify-center my-7">
+          <CommunityFunds id={2} balance={communityBalance} royalty={royalty} />
+        </div>
+        {/* <div className="flex flex-col items-center justify-between relative">
           <div className="relative sm:absolute sm:left-[-20px] sm:top-[-20px]">
             <div className="w-[170px] h-[85px]">
               <KitsLogo />
@@ -66,7 +89,6 @@ const Header = ({
         </div>
         <div className="flex flex-col items-center gap-5 my-5">
           <CommunityFunds id={2} balance={communityBalance} royalty={royalty} />
-          {/* <Jackpot jackpot={14.4} /> */}
           <PlayerFunds
             won={won}
             prize={prize}
@@ -76,7 +98,7 @@ const Header = ({
           <div className="flex gap-2">
             <SolBalance id={4} balance={solBalance} lost={lost} bet={bet} />
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
