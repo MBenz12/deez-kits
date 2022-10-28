8ab60d50849614f58874d51232ddf71a51b13d18
Dharmesh_P31 <dharmesh_p@aol.com>
2022-10-28 15:36:46 +0530

    add home button link and other navlink in footer on mint and countdown page

--- src/sharedComponent/footer/footer.jsx
+++ src/sharedComponent/footer/footer.jsx
@@ -3,6 +3,9 @@ import style from './footer.module.scss'
 import meIcon from "../../assets/images/deezKits/me.svg";
 import twitterIcon from "../../assets/images/deezKits/twitter.svg";
 import discordIcon from "../../assets/images/deezKits/discord.svg";
+import HomeIcon from '../../assets/images/deezKits/home_kitz.svg'
+import kitIcon from "../../assets/images/cat.gif";
+import coinFlipIcon from "../../assets/images/coinflip.png";
 const footer = () => {
 
   return (
@@ -34,7 +37,42 @@ const footer = () => {
           <img src={discordIcon} alt="discord-icon" />
           Discord
         </a>
+        <a
+            href="https://staking.deezkits.com"
+            rel="noreferrer"
+            className={style.staking_link}
+        >
+          <img src={kitIcon} alt="staking-icon" />
+          Staking
+        </a>
+
+        <a
+            href="https://slotz.deezkits.com"
+            rel="noreferrer"
+            className={style.staking_link}
+        >
+          <span>🎰</span>
+          <span>Slotz</span>
+        </a>
+
+        <a
+            href="https://coinflip.deezkits.com"
+            rel="noreferrer"
+            className={style.coinflip_link}
+        >
+          <img src={coinFlipIcon} alt="coinflip-icon" />
+          <span>Coin Flip</span>
+        </a>
+        <a
+            href="https://www.deezkits.com/"
+            rel="noreferrer"
+            className={style.coinflip_link}
+        >
+          <img src={HomeIcon} alt="coinflip-icon" />
+          <span>Home</span>
+        </a>
       </div>
+
   )
 };
 
