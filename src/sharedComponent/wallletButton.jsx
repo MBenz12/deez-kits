f0c842920d2057aea5a1d485d7c6c5d6052a24fa
Dharmesh_P31 <dharmesh_p@aol.com>
2022-11-01 10:41:36 +0530

    fix wallet button issue in mobile screen and use default wallet button in mint page

--- src/sharedComponent/wallletButton.jsx
+++ src/sharedComponent/wallletButton.jsx
@@ -1,43 +1,46 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import {
-  WalletDisconnectButton, WalletMultiButton
+  WalletDisconnectButton,
+  WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import { useEffect } from "react";
-import { toast } from 'react-toastify';
-import style from '../assets/css/wallet.module.scss';
+import { toast } from "react-toastify";
+import style from "../assets/css/wallet.module.scss";
 const WalletButton = () => {
-
   const wallet = useWallet();
   useEffect(() => {
-   if(wallet?.publicKey?.toString() && wallet.connected){
-    // console.log();
-    toast.dismiss();
-    toast.success("Wallet Connected Successfully.",{position: "bottom-right",theme: "dark",
-    autoClose: 3000,})
+    if (wallet?.publicKey?.toString() && wallet.connected) {
+      // console.log();
+      toast.dismiss();
+      toast.success("Wallet Connected Successfully.", {
+        position: "bottom-right",
+        theme: "dark",
+        autoClose: 3000,
+      });
       // console.log(wallet?.publicKey?.toString());
-   }
+    }
   }, [wallet.connected]);
 
   const handleDisconnect = () => {
     toast.dismiss();
-    toast.success("Wallet Disconnected Successfully.",{position: "bottom-right",theme: "dark",
-    autoClose: 3000,})
+    toast.success("Wallet Disconnected Successfully.", {
+      position: "bottom-right",
+      theme: "dark",
+      autoClose: 3000,
+    });
     // console.log("Disconnecting from wallet");
   };
 
   return (
-    <div
-      className={style.connect_wallet_wrapper}
-    >
+    <div className={style.connect_wallet_wrapper}>
       <div>
         <div>
           {wallet.connected ? (
-            <div
-              className={style.wallet_wrapper}
-            >
-              <WalletDisconnectButton onClick={handleDisconnect} className={style.diss_connect}>
-                {wallet?.publicKey?.toString().slice(0,4)}....{wallet?.publicKey?.toString().substr(-4)}
-              </WalletDisconnectButton>
+            <div className={style.wallet_wrapper}>
+              <WalletMultiButton className={style.wallet_connect}>
+                {wallet?.publicKey?.toString().slice(0, 4)}....
+                {wallet?.publicKey?.toString().substr(-4)}
+              </WalletMultiButton>
             </div>
           ) : (
             <WalletMultiButton className={style.wallet_connect}>
