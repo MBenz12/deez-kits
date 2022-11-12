f31555191e4372b95159cf9bbbe539f9161eda25
Koby Ahronson <kobyle69@gmail.com>
2022-11-12 17:32:03 +0200

    - Fixing pages CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,6 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    screens: {
-      'xl': { 'max': '1600px' },
-      'lg': { 'max': '1200px' },
-      'md': { 'max': '992px' },
-      'sm': { 'max': '768px' },
-      'xs': { 'max': '576px' },
-      '2xl': '1600px',
-    },
-    colors: {
-      'primary': '#7A58FF',
-      'theme': '#83FF49',
-    },
-    extend: {
-    }
   },
   plugins: [],
 }
