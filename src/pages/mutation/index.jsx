b46f5ff774df49e747104890726c66b25e612011
Lucas Wong <soso.live.co@outlook.com>
2022-11-16 06:02:37 -0500

    implement popup modal when clicking items to mutate

--- src/pages/mutation/index.jsx
+++ src/pages/mutation/index.jsx
@@ -23,8 +23,61 @@ import './glitch.css';
 import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
 import { Metaplex, keypairIdentity } from '@metaplex-foundation/js';
 import { useWallet } from '@solana/wallet-adapter-react';
+import { Modal } from '@mui/material';
+
+const items = [
+	{
+		tokenId: 1,
+		type: 'kit',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 2,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 3,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 4,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 5,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 6,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 7,
+		type: 'sardine',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 8,
+		type: 'mouse',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+	{
+		tokenId: 9,
+		type: 'mouse',
+		imgUrl: 'https://arweave.net/nLnbcCu3UleLFxDPnGfhH4GvM7nKoHHWodVMqrNlYqc?ext=png',
+	},
+];
 
 const Mutation = () => {
+	const [open, setOpen] = useState(false);
+	const [NFTdata, setNFTdata] = useState([]);
+	const [selectedType, setSelectedType] = useState('');
+	const [mutateNFTs, setMutateNFTs] = useState({});
 	const videoRef = useRef();
 
 	const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
@@ -42,23 +95,64 @@ const Mutation = () => {
 			const data = await metaplex
 				.nfts()
 				.findByMint({ mintAddress: new PublicKey('9LcAHBasMj9TavyuN1JnbDWXkmofnkom3J4T4sL6xjiN') });
-			console.log(data);
 			setNFTs(data);
 		};
 		getNFTs();
 	}, [wallet.publicKey]); //eslint-disable-line
 
-	console.log(NFTs);
-
 	const handleMutate = (index) => {
 		if (index === 0) {
+			setSelectedType('kit');
 		} else if (index === 1) {
+			setSelectedType('sardine');
 		} else if (index === 2) {
+			setSelectedType('mouse');
 		}
+		setOpen(true);
+	};
+
+	useEffect(() => {
+		if (selectedType === '') return;
+		const temp = items.filter((item) => item.type === selectedType);
+		setNFTdata(temp);
+	}, [selectedType]);
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleSelect = (nft) => {
+		if (nft.type === 'sardine') setMutateNFTs((prev) => ({ ...prev, sardine: nft }));
+		if (nft.type === 'mouse') setMutateNFTs((prev) => ({ ...prev, mouse: nft }));
+		if (nft.type === 'kit') setMutateNFTs((prev) => ({ ...prev, kit: nft }));
+
+		handleClose();
+	};
+
+	const handleMutateNFTs = () => {
+		console.log(mutateNFTs);
 	};
 
 	return (
 		<div className='relative overflow-x-hidden flex flex-col font-mutation'>
+			<Modal open={open} onClose={handleClose}>
+				<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-auto border-2 border-primary rounded-lg outline-none max-w-[800px] max-h-[80vh] w-full p-4 bg-[#1B1248]'>
+					<h1 className='text-4xl text-white text-center mb-4'>Choose NFT to mutate</h1>
+					<div className='grid grid-cols-4 _md:grid-cols-3 _sm:grid-cols-2 gap-3'>
+						{NFTdata !== [] &&
+							NFTdata.map((nft, index) => (
+								<div
+									className='flex flex-col justify-center overflow-hidden border-2 border-primary rounded-lg text-2xl text-white text-center'
+									onClick={() => handleSelect(nft)}
+									key={index}
+								>
+									<img alt='nft-art' src={nft.imgUrl} key={index} className='w-full object-cover' />
+									{nft.type} - {nft.tokenId}
+								</div>
+							))}
+					</div>
+				</div>
+			</Modal>
 			<WalletMultiButton className='absolute top-2 _sm:top-16 right-2 bg-primary' />
 			<a href='/' className='absolute z-20 top-0 left-0 flex items-center h-[104px] w-auto'>
 				<img src={Logo} alt='Logo' className='w-full h-full' />
@@ -92,17 +186,38 @@ const Mutation = () => {
 					<p className='_sm:mx-auto text-[44.69px] _sm:text-[35.75px] text-theme'>MUTATION</p>
 					<div className='flex _sm:flex-col gap-4 mt-8 items-center _sm:px-12'>
 						<div className='w-full'>
-							<div className='overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-_md aspect-square'>
-								<img src={Mutation1} alt='' className='scale-75 w-full h-full object-contain' />
+							<div className='group relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-md p-2 aspect-square'>
+								<img
+									src={mutateNFTs?.kit?.imgUrl || Mutation1}
+									alt=''
+									className={`w-full h-full object-contain rounded-md group-hover:hidden ${
+										!mutateNFTs?.kit?.imgUrl && 'scale-75'
+									}`}
+								/>
+								<div
+									className='hidden absolute z-10 top-0 left-0 group-hover:flex flex-col items-center justify-center gap-4 border-2 border-[#683CB6] rounded-2xl w-full h-full bg-[#20194D] text-xl font-bold text-[#683CB6] text-center'
+									onClick={() => {
+										handleMutate(0);
+									}}
+								>
+									<div className='flex items-center justify-center rounded-md w-20 h-20 _md:w-12 _md:h-12 bg-[#683CB6]'>
+										<PlusIcon className='w-8 h-8 _md:w-4 _md:h-4 fill-path' />
+									</div>
+									Choose your Kit
+								</div>
 							</div>
-							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-_md w-full h-12 text-[25px] text-[#952CFF]'>
+							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-md w-full h-12 text-[25px] text-[#952CFF]'>
 								KIT
 							</button>
 						</div>
 						<PlusIcon className='-mt-[48px] _sm:mt-0 max-w-[24px] max-h-[24px] min-w-[24px] min-h-[24px]' />
 						<div className='w-full'>
-							<div className='group relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-_md aspect-square'>
-								<img src={Mutation2} alt='' className='w-full h-full object-contain group-hover:hidden' />
+							<div className='group relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-md p-2 aspect-square'>
+								<img
+									src={mutateNFTs?.sardine?.imgUrl || Mutation2}
+									alt=''
+									className='w-full h-full object-contain rounded-md group-hover:hidden'
+								/>
 								<div
 									className='hidden absolute z-10 top-0 left-0 group-hover:flex flex-col items-center justify-center gap-4 border-2 border-[#683CB6] rounded-2xl w-full h-full bg-[#20194D] text-xl font-bold text-[#683CB6] text-center'
 									onClick={() => {
@@ -115,26 +230,43 @@ const Mutation = () => {
 									Choose your Sardine
 								</div>
 							</div>
-							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-_md w-full h-12 text-[25px] text-[#952CFF]'>
+							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-md w-full h-12 text-[25px] text-[#952CFF]'>
 								SARDINE
 							</button>
 						</div>
 						<PlusIcon className='-mt-[48px] _sm:mt-0 max-w-[24px] max-h-[24px] min-w-[24px] min-h-[24px]' />
 						<div className='w-full'>
-							<div className='overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-_md aspect-square'>
-								<img src={Mutation3} alt='' className='scale-75 w-full h-full object-contain' />
+							<div className='group relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-[#7D7D7D] rounded-md p-2 aspect-square'>
+								<img
+									src={mutateNFTs?.mouse?.imgUrl || Mutation3}
+									alt=''
+									className={`w-full h-full object-contain rounded-md group-hover:hidden ${
+										!mutateNFTs?.mouse?.imgUrl && 'scale-75'
+									}`}
+								/>
+								<div
+									className='hidden absolute z-10 top-0 left-0 group-hover:flex flex-col items-center justify-center gap-4 border-2 border-[#683CB6] rounded-2xl w-full h-full bg-[#20194D] text-xl font-bold text-[#683CB6] text-center'
+									onClick={() => {
+										handleMutate(2);
+									}}
+								>
+									<div className='flex items-center justify-center rounded-md w-20 h-20 _md:w-12 _md:h-12 bg-[#683CB6]'>
+										<PlusIcon className='w-8 h-8 _md:w-4 _md:h-4 fill-path' />
+									</div>
+									Choose your Mouse
+								</div>
 							</div>
-							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-_md w-full h-12 text-[25px] text-[#952CFF]'>
+							<button className='mt-6 border-[#952CFF] border-[1.65px] rounded-md w-full h-12 text-[25px] text-[#952CFF]'>
 								MOUSE
 							</button>
 						</div>
 						<EqualsIcon className='-mt-[48px] _sm:mt-0 max-w-[24px] max-h-[24px] min-w-[24px] min-h-[24px]' />
 						<div className='w-full'>
-							<div className='relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-theme rounded-_md aspect-square'>
+							<div className='relative overflow-hidden flex items-center justify-center border-dashed border-[1.78px] border-theme rounded-md aspect-square'>
 								<img src={Mutation4} alt='' className='w-full h-full object-contain' />
 								<img src={Mutation5} alt='' className='absolute top-0 left-0' />
 							</div>
-							<button className='relative mt-6 w-full h-12'>
+							<button className='relative mt-6 w-full h-12' onClick={handleMutateNFTs}>
 								<img src={Mutate} alt='' className='absolute -top-1/3 left-0	w-full' />
 							</button>
 						</div>
